2ae9c67bc8251e25e369e0db10f9b77a78eed941
joonas <joonas.yli-saari@fidera.fi>
2023-12-26 23:32:50 +0200

    exercises until 2.8

--- osa2/puhelinluettelo/src/App.jsx
+++ osa2/puhelinluettelo/src/App.jsx
@@ -1,35 +1,66 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+const Person = ({person}) => {
+    return(
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+            {person.name} {person.phoneNumber}
         </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    )
+}
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: 'Arto Hellas', number: '040-123456' },
+        { name: 'Ada Lovelace', number: '39-44-5323523' },
+        { name: 'Dan Abramov', number: '12-43-234345' },
+        { name: 'Mary Poppendieck', number: '39-23-6423122' }
+    ])
+
+    const [newPerson, setNewPerson] = useState({
+        name: '', phoneNumber: ''
+    })
+
+    const addNewPerson = (event) => {
+        event.preventDefault();
+        if (persons.filter(person => person.name === newPerson.name)?.length > 0 ) {
+            alert(`${newPerson.name} is already in notebook`)
+        } else {
+            const newPersons = persons.concat({...newPerson})
+            setPersons(newPersons)
+            setNewPerson({
+                name: '', phoneNumber: ''
+            })
+        }
+    }
+
+    const handleNameChange = (event) => {
+        setNewPerson({...newPerson, name: event.target.value})
+    }
+    const handleNumberChange = (event) => {
+        setNewPerson({...newPerson, phoneNumber: event.target.value})
+    }
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addNewPerson}>
+                <div>
+                    name: <input value={newPerson.name} onChange={handleNameChange}/>
+                </div>
+                <div>
+                    number: <input value={newPerson.phoneNumber} onChange={handleNumberChange}/>
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {
+                persons.map(person => <Person person={person} key={person.name}></Person> )
+            }
+        </div>
+    )
+
 }
 
 export default App
