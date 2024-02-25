ebb22ea73febfa065f1373e9c06bd57a8a6a6e7b
joonas <joonas.yli-saari@fidera.fi>
2024-02-25 21:36:55 +0200

    added cors and nodemon and blogController

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -4,8 +4,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const logger = require('./utils/logger')
 const { PORT, MONGODB_URI } = require('./utils/config')
-const Blog = require('./models/blog')
-
+const blogRouter = require('./controllers/blogs')
 
 mongoose.connect(MONGODB_URI)
     .then(() => {
@@ -17,22 +16,8 @@ mongoose.connect(MONGODB_URI)
 
 app.use(cors())
 app.use(express.json())
+app.use('/api/blogs', blogRouter)
 
-app.get('/api/blogs', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-})
-
-app.post('/api/blogs', (request, response) => {
-    const blog = new Blog(request.body)
-    blog.save()
-        .then(result => {
-            response.status(201).json(result)
-        })
-})
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
